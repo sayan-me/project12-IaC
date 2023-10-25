5f2fb11dba2cfba56fba7a371be4bcd40a088693
Sayan Das <dsayan154@gmail.com>
2023-10-25 09:05:48 +0530

    setting env vars in action step

--- .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.sh
+++ .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import pyotp, os
-AWS_MFA_TOTP_KEY = os.environ.get("$INPUT_MFA-SECRET-KEY")
+AWS_MFA_TOTP_KEY = os.environ.get("AWS_MFA_SECRET_KEY")
 totp = pyotp.TOTP(AWS_MFA_TOTP_KEY)
 print(f"AWS_MFA_OTP={totp.now()}")
