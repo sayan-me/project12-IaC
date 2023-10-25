4c0fcd93554783aa27f4db57f1efc4d33aa7ee6a
Sayan Das <dsayan154@gmail.com>
2023-10-25 07:02:23 +0530

    restructing the python script

--- .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.py
+++ .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.py
@@ -1,3 +1,4 @@
 import pyotp
 AWS_MFA_TOTP_KEY = "{{ secrets.AWS_MFA_TOTP_KEY }}"
-print(f"AWS_MFA_OTP={pyotp.TOTP(AWS_MFA_TOTP_KEY).now()}")
+totp = pyotp.TOTP(AWS_MFA_TOTP_KEY)
+print(f"AWS_MFA_OTP={totp.now()}")
