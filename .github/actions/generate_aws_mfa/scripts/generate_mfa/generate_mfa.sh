731f876a952cc9b3052b6b26e8fb2d3c424ffbec
Sayan Das <dsayan154@gmail.com>
2023-10-25 08:11:28 +0530

    fixed accessing env var

--- .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.sh
+++ .github/actions/generate_aws_mfa/scripts/generate_mfa/generate_mfa.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import pyotp
-AWS_MFA_TOTP_KEY = "$INPUT_MFA-SECRET-KEY"
+import pyotp, os
+AWS_MFA_TOTP_KEY = os.environ.get("$INPUT_MFA-SECRET-KEY")
 totp = pyotp.TOTP(AWS_MFA_TOTP_KEY)
 print(f"AWS_MFA_OTP={totp.now()}")
