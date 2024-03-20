edb635b414b6378b73df4b4fa8f19b09cf914fdf
Sayan Das <dsayan154@gmail.com>
2024-03-20 06:31:16 +0530

    added the terraform code to create gha_integration

---
+++ terraform/core/modules/gha-oidc/terraform.tf
@@ -0,0 +1,21 @@
+variable "env" {
+  type = string
+  description = "environment type"
+}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+}
+provider "aws" {
+  default_tags {
+    tags = {
+      "create-by" = "terraform"
+      "tf-module" = "gha-oidc"
+      "env" = var.env
+    }
+  }
+}
